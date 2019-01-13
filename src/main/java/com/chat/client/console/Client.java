871273094dff8d8dc0b93d3f19ee397996761519
Willy Aguirre <marti1125@gmail.com>
2019-01-12 20:33:38 -0500

    separete into files #1

--- src/main/java/com/chat/client/console/Client.java
+++ src/main/java/com/chat/client/console/Client.java
@@ -1,14 +1,10 @@
-package com.chat;
+package com.chat.client.console;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.io.InputStream;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
-import java.io.StringReader;
 
 public class Client {
 
@@ -56,41 +52,6 @@ public class Client {
     sc.close();
     client.close();
   }
-}
-
-class ReceivedMessagesHandler implements Runnable {
-
-  private InputStream server;
-
-  public ReceivedMessagesHandler(InputStream server) {
-    this.server = server;
-  }
-
-  public void run() {
-    // receive server messages and print out to screen
-    Scanner s = new Scanner(server);
-    String tmp = "";
-    while (s.hasNextLine()) {
-      tmp = s.nextLine();
-      if (tmp.charAt(0) == '[') {
-        tmp = tmp.substring(1, tmp.length()-1);
-        System.out.println(
-            "\nUSERS LIST: " +
-            new ArrayList<String>(Arrays.asList(tmp.split(","))) + "\n"
-            );
-      }else{
-        try {
-          System.out.println("\n" + getTagValue(tmp));
-          // System.out.println(tmp);
-        } catch(Exception ignore){}
-      }
-    }
-    s.close();
-  }
-
-  // I could use a javax.xml.parsers but the goal of Client.java is to keep everything tight and simple
-  public static String getTagValue(String xml){
-    return  xml.split(">")[2].split("<")[0] + xml.split("<span>")[1].split("</span>")[0];
-  }
 
 }
+
